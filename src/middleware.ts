29fefeef2c9629e6b34c49bc9353b276b1629b76
Akshay Ajith <akshay.ajith@kalkitech.com>
2025-10-31 17:41:08 +0530

    cleanup : removed unused

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,6 @@
 import { withAuth } from "next-auth/middleware";
 
 export default withAuth(
-  function middleware(_req) {
-    // Add any additional middleware logic here
-  },
   {
     callbacks: {
       authorized: ({ token }) => !!token,
